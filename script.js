72c51842f2ac597df7fc968e3fdd4d79b059108d
dlin <lindevin101@gmail.com>
2023-10-12 10:06:54 -0400

    created class and loop

---
+++ script.js
@@ -0,0 +1,5 @@
+let Board = document.getElementsByClassName("Board")
+let Box = document.getElementsByClassName("box")
+for (let i = 0; i < Box.length; i++) {
+    Box[i]. addEventListener("click", doThis)
+}
