ca757ae6b879463f76990cf46ddb76b701cf87d1
dlin <lindevin101@gmail.com>
2023-10-12 11:10:32 -0400

    index.html done

--- script.js
+++ script.js
@@ -1,60 +1,70 @@
 let Board = document.getElementsByClassName("Board")
 let Box = document.getElementsByClassName("box")
-for (let i = 0; i < Box.length; i++) {
-    Box[i]. addEventListener("click", doThis)
+for (let i = 0; i < Box.length; i++){
+  Box[i].addEventListener("click", doThis)
 }
-let possibilites = [["a1","a2","a3"],["b1","b2","b3"],["c1","c2","c3"],["c1", "b2", "a3"], ["a1", "b2", "c3"], ["a1", "b1", "c1"], ["a2", "b2", "c2"], ["a3", "b3", "c3"]]
+let possibilites = [["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1", "c2", "c3"], ["c1", "b2", "a3"], ["a1", "b2", "c3"], ["a1", "b1", "c1"], ["a2", "b2", "c2"], ["a3", "b3", "c3"]]
 let player = 0
 function doThis() {
-if (player === 0) {
-    document.getElementById(event.target.id).style.backgroundColor = "black";
+  if (player === 0) {
+    document.getElementById(event.target.id).style.backgroundColor = "blue";
     event.target.dataset.type = "clicked"
     for (let i = 0; i < possibilites.length; i++) {
-        for(let j = 0; j < possibilites[i].length; j++) {
-            if (possibilites[i][j]=== event.target.id) {
-                possibilites[i][j] = "black"
-            }
+      for (let j = 0; j < possibilites[i].length; j++) {
+        if (possibilites[i][j] === event.target.id) {
+          possibilites[i][j] = "blue"
         }
+      }
     }
-    whoTurn.innerText = "Green's Turn"
-    checkWin()
+    whosTurn.innerText = "Red's Turn"
+    checkWIN()
     player = 1
-}
-else {
-    document.getElementById(event.target.id).style.backgroundColor = "green";
+  }
+  else {
+    document.getElementById(event.target.id).style.backgroundColor = "red";
     event.target.dataset.type = "clicked"
-    for (let i = 0; i< possibilites.length; i++) {
-        for (let j = 0; j< possibilites[i].length; j++)
-        if(possibilites[i][j] === event.target.id) {
-            possibilites[i][j] = "green"
-        }
-    }
-}
-whoTurn.innerText = "Black's Turn"
-checkWin()
-player = 0
-}
-function checkWin () {
-    counter  = 0
     for (let i = 0; i < possibilites.length; i++) {
-        if (possibilites[i][0] === "black" && possibilites[i][1] = "black" && possibilites[1][2]=== "black") {
-            modelContent.innerText = "Black Wins"
-            ToggleModel()
+      for (let j = 0; j < possibilites[i].length; j++) {
+        if (possibilites[i][j] === event.target.id) {
+          possibilites[i][j] = "red"
         }
-        if (possibilites[i][0] === "green" && possibilites[i][1] = "green" && possibilites[1][2]=== "black") {
-            modelContent.innerText = "Green Wins"
-            ToggleModel()
-        }
+      }
     }
-    for (let i = 0; i < Box.length; i++) {
-        if (Box[i].dataset.type === "clicked") {
-            counter++
-        }
-        if(counter===9) {
-            modal.Content.innerText = "Its a Tie"
-            counter = 0
-            ToggleModel()
-        }
+    whosTurn.innerText = "Blue's Turn"
+    checkWIN()
+    player = 0
+  }
+  function checkWIN() {
+    counter = 0
+    for (let i = 0; i < possibilites.length; i++) {
+      if (possibilites[i][0] === "blue" && possibilites[i][1] === "blue" && possibilites[i][2] === "blue") {
+        modalContent.innerText = "Blue Wins"
+        ToggleModal()
+      }
+      if (possibilites[i][0] === "red" && possibilites[i][1] === "red" && possibilites[i][2] === "red") {
+        modalContent.innerText = "Red Wins"
+        ToggleModal()
+      }
     }
+    for (let i = 0; i < Box.length; i++){
+      if (Box[i].dataset.type === "clicked") {
+        counter++
+      }
+      if (counter === 9) {
+        modalContent.innerText = "Its a Tie"
+        counter = 0
+        ToggleModal()
+      }
+    }
+  }
+}
 
+function reset() {
+  for (let i = 0; i < Box.length; i++){
+    Box[i].style.backgroundColor = "#ffff3"
+  }
+  possibilites = [["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1", "c2", "c3"], ["c1", "b2", "a3"], ["a1", "b2", "c3"], ["a1", "b1", "c1"], ["a2", "b2", "c2"], ["a3", "b3", "c3"]]
+  for (let i = 0; i < Box.length; i++){
+    Box[i].dataset.type = "poop"
+  }
 }
